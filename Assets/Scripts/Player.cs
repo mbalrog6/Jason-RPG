e14a1f6a5b2cde858b43be91f2d79dc237ce8845
Scott Heinrichs <mbalrog6@gmail.com>
2020-05-30 18:58:15 -0700

    Added IMover Interface. Created NavmeshMovement class.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,32 +1,20 @@
-﻿using UnityEngine;
+﻿using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private CharacterController _characterController;
+    private IMover _mover;
     public IPlayerInput ControllerInput { get; set; } = new PlayerInput();
 
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
-        //_playerInput = new PlayerInput();
+        _mover = new NavMeshMover(this );
     }
 
     private void Update()
     {
-        Vector3 movementInput = new Vector3(ControllerInput.Horizontal, 0, ControllerInput.Vertical);
-        Vector3 movement = transform.rotation * movementInput;
-        _characterController.SimpleMove(movement);
+        _mover.Tick();
     }
-}
-
-public class PlayerInput : IPlayerInput
-{
-    public float Vertical => Input.GetAxis("Vertical");
-    public float Horizontal => Input.GetAxis("Horizontal");
-}
-
-public interface IPlayerInput
-{
-    float Vertical { get; }
-    float Horizontal { get; }
 }
